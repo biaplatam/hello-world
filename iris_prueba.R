3a05dd7bc6bf5f2e1c983762cd5a90196794c958
biaplatam <bi@aplatam.com>
2019-03-29 11:18:48 -0600

    Agregue grafica nueva

--- iris_prueba.R
+++ iris_prueba.R
@@ -1,2 +1,6 @@
 library(ggplot2)
 ggplot(iris,aes(Sepal.Length,Sepal.Width,color=Species)) + geom_jitter()
+
+###Cambios
+
+ggplot(iris,aes(Sepal.Length,Sepal.Width,color=Species)) + geom_jitter() + geom_smooth(method="lm")
