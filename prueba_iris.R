753d476f03daa418580b18b7220c96803227a492
biaplatam <48567832+biaplatam@users.noreply.github.com>
2019-03-29 11:09:32 -0600

    Delete prueba_iris.R

--- prueba_iris.R
+++
@@ -1,8 +0,0 @@
-#hola Mundo
-
-library(ggplot2)
-ggplot(iris,aes(Sepal.Length,Sepal.Width,color=Species)) + geom_jitter() + geom_smooth(method="lm")
-
-
-###Cambios
-ggplot(iris,aes(Sepal.Length,Sepal.Width,color=Species)) + geom_jitter() + geom_density2d()
